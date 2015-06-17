43d684208967004fbcaf65e6865e499a57a973bf
JH <victoryjh@dreamwiz.com>
2015-06-17 17:33:30 +0900

    dd

--- src/main/java/com/jh/Test.java
+++ src/main/java/com/jh/Test.java
@@ -3,7 +3,8 @@ package com.jh;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("cccccccccc");
+		System.out.println("A");
+		System.out.println("B");
 	}
 
 }
